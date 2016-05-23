9604792acbdbc7b7888ab88d4d5420acf763db36
Pablonovillo96 <novalauto1A>
2016-05-23 16:10:05 +0200

    ahora solo lo dice una vez

--- EDPRiueba/src/EXAMEN.java
+++ EDPRiueba/src/EXAMEN.java
@@ -2,8 +2,8 @@
 public class EXAMEN {
 
 	public static void main(String[] args) {
-		for(int i=0;i<10000;i++) System.out.println("PESADOS DE MIERDA");
-
+		for(int i=0;i<10000;i++) i=i+100000; System.out.println("PESADOS DE MIERDA");
+  
 	}
 
 }
